a6294e0429446ae85b72f100793d503fa6d287fc
Rob Weber <rob.weber87@gmail.com>
2018-10-15 23:13:42 -0400

    added document ready when app is loaded

--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -1,3 +1,5 @@
+var socket = io.connect();
+
 function divEscapedContentElement(message) {
 	return $('<div></div>').text(message);
 };
@@ -20,3 +22,54 @@ function processUserInput(chatApp, socket) {
 	}
 	$('#send-message').val('');
 };
+
+$(document).ready(function() {
+	var chatApp = new Chat(socket);
+
+	socket.on('nameResult', function(result) {
+		var message;
+		if (result.success) {
+			message = 'You are now known as ' + result.name + '.';
+		} else {
+			message = result.message;
+		}
+		$('#messages').append(divSystemContentElement(message));
+	});
+
+	socket.on('jointResult', function(result) {
+		$('#room').text(result.room);
+		$('#messages').append(divSystemContentElement('Room changed.'));
+	});
+
+	socket.on('message', function(message) {
+		var newElement = $('<div></div>').text(message.text);
+		$('#messages').append(newElement);
+	});
+
+	socket.on('rooms', function(rooms) {
+		$('#room-list').empty();
+
+		for(var room in rooms) {
+			room = room.substring(1, room.length);
+			if (room != '') {
+				$('#room-list').append(divSystemContentElement(room));
+			}
+		}
+
+		$('#room-list div').click(function() {
+			chatApp.processCommand('/join ' + $(this).text());
+			$('#send-message').focus();
+		});
+	});
+
+	setInterval(function() {
+		socket.emit('rooms');
+	}, 1000);
+
+	$('#send-message').focus();
+
+	$('#send-form').submit(function() {
+		processUserInput(chatApp, socket);
+		return false;
+	});
+});
